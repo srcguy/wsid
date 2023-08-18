8375626a9af31cdd18415cb086c432d10e2d1141
Bruno S <81621691+srcguy@users.noreply.github.com>
2023-08-18 02:09:55 +0200

    Add files via upload

--- WSID.py
+++ WSID.py
@@ -1,4 +1,4 @@
-#What should I do? (WSID) beta 2 by src_guy
+#What should I do? (WSID) beta 3 by src_guy
 #2023
 
 from tkinter import *
@@ -46,8 +46,33 @@ def sticky():
     Button(sticky_menu, text="Create new sticky note", command=new_sticky).place(x=0, y=150)
     Button(sticky_menu, text="Exit", bg="red", command=sticky_menu.destroy).place(x=0, y=175)
 
+def todo_save():
+    file = open("todo.txt", "w")
+    file.write(todo_content.get("1.0","end"))
+    file.close()
+
+def todo():
+    todo_menu = Toplevel(root)
+    todo_menu.geometry("300x209")
+    todo_menu.title("To do creator")
+    Label(todo_menu, text="To do creator", font=("David", 16)).place(x=0, y=9)
+    global todo_content
+    todo_content = Text(todo_menu, width=30, height=5)
+    todo_content.place(x=0, y=50)
+    Button(todo_menu, text="Save", command=todo_save).place(x=0, y=150)
+    Button(todo_menu, text="Exit", command=todo_menu.destroy, bg="red").place(x=50, y=150)
+
+def todo_list():
+    todo_menu_list = Toplevel(root)
+    todo_menu_list.geometry("300x209")
+    todo_menu_list.title("To do list")
+    file = open("todo.txt", "r")
+    Label(todo_menu_list, text=file.read()).place(relx=0.5, rely=0.5, anchor=CENTER)
+
 Label(root, text="What should I do?", font=("System", 24)).place(x=0, y=2)
 Button(root, text="Sticky notes", command=sticky).place(x=0, y=50)
-Button(root, text="Exit", bg="red", command=root.destroy).place(x=0, y=100)
+Button(root, text="To do creator", command=todo).place(x=0, y=100)
+Button(root, text="To do list", command=todo_list).place(x=100, y=100)
+Button(root, text="Exit", bg="red", command=root.destroy).place(x=0, y=150)
 
 root.mainloop()
